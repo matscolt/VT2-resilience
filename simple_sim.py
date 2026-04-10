2f82d0c1956a24251000c46187e775f9d06b4a02
Matscolt <mathias.terndrup@live.dk>
2026-04-10 13:30:01 +0200

    Creating a simulation file

---
+++ simple_sim.py
@@ -0,0 +1,15 @@
+#This is going to be a simple simulation of the FESTO line.
+# There is going to be a selection of modules and there process times based on the recorded data.
+
+#import packages
+
+#define modules and process times
+
+#define the logic of the line
+
+#main loop
+def main():
+    print("Main loop")
+
+if __name__ == "__main__":
+    main()
