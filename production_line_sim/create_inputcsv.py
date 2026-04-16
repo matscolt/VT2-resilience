7bac7cfea079071ddcd1841b1445eb9dd806fa44
Matscolt <mathias.terndrup@live.dk>
2026-04-16 09:42:44 +0200

    Added csv generator

---
+++ production_line_sim/create_inputcsv.py
@@ -0,0 +1,49 @@
+from pathlib import Path
+import csv
+import random
+import re
+from datetime import datetime
+
+# 🔷 Generate orders
+def generate_order():
+    return [{
+        "order_id": "O1",
+        "variant": "FUSE0",   # manually chosen
+        "quantity": 1,        # fixed number
+        "start_time": 0,
+        "priority": 1         # fixed priority
+    }]
+
+# 🔷 Write CSV
+def write_csv(rows, output_path: Path):
+    with output_path.open("w", newline="", encoding="utf-8") as f:
+        writer = csv.DictWriter(
+            f,
+            fieldnames=["order_id", "variant", "quantity", "start_time", "priority"]
+        )
+        writer.writeheader()
+        writer.writerows(rows)
+
+
+# 🔷 MAIN
+def main():
+    base_dir = Path(__file__).resolve().parent
+    input_dir = base_dir / "input"
+
+    input_dir.mkdir(exist_ok=True)
+
+    timestamp = datetime.now().strftime("%m-%d_%H-%M")
+    ordername = f"orders_{timestamp}_1.csv"
+    while (input_dir / ordername).exists():
+        ordername = f"orders_{timestamp}_{int(ordername.split('_')[-1].removesuffix('.csv')) + 1}.csv"
+    output_path = input_dir / ordername
+
+    rows = generate_order()
+    write_csv(rows, output_path)
+    print(f"--- Created order file: {ordername} ----")
+
+
+if __name__ == "__main__":
+    main()
+
+
