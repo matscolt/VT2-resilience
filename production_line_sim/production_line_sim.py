8a0646454bfcc1553a4705ce0a595d61df0fc821
Matscolt <mathias.terndrup@live.dk>
2026-04-16 18:03:24 +0200

    second transport seems to be funky - with 9 units (3 of each) it seems the robotcell does not start as it should

--- production_line_sim/production_line_sim.py
+++ production_line_sim/production_line_sim.py
@@ -452,6 +452,7 @@ def disruptions(seed):
     #then it should generate the disruption chance for each operation within the given range
     #it should be able to also check every time a dummy phone reaches a station if there should be a disruption or not
 
+    #NOTE TRANSPORT 2 LOOKS WRONG
 
 
 # -----------------------------
