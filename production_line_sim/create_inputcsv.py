34b39352bc61b008b50f1270f441d85e82b34655
maci0066 <maciej55511@gmail.com>
2026-04-16 10:56:22 +0200

    Added input logic for orders, settings and disrupt

--- production_line_sim/create_inputcsv.py
+++ production_line_sim/create_inputcsv.py
@@ -2,7 +2,9 @@ from pathlib import Path
 import csv
 import random
 import re
+import json
 from datetime import datetime
+
 
 # 🔷 Generate orders
 def generate_order():
@@ -10,13 +12,37 @@ def generate_order():
         "order_id": 1,
         "Order_time": 0,
         "priority": 1,
-        "variant0": "FUSE0",   
-        "quantity": 0,
+        "variant0": "FUSE0",
         "variant1": "FUSE1",   
-        "quantity": 0,
         "variant2": "FUSE2",
         "quantity": 0
         }]
+
+#Create settings JSON file
+def create_json(output_path: Path):
+    Setting = {
+        "Sim_time [s]" :    3600
+        }
+    with output_path.open("w", encoding="utf-8") as f:
+        json.dump(Setting, f, indent=4) 
+
+#Create settings JSON file
+def create_setting_json(output_path: Path):
+    Setting = {
+        "Sim_time [s]" :    3600
+        }
+    with output_path.open("w", encoding="utf-8") as f:
+        json.dump(Setting, f, indent=4) 
+
+#Create disruption JSON file
+def create_disruption_json(output_path: Path):
+    Setting = {
+        "station"
+            "Machine breakdown chance [%]" :    0,
+            "Machine breakdown duration [s]" :  0,
+        }
+    with output_path.open("w", encoding="utf-8") as f:
+        json.dump(Setting, f, indent=4) 
 
 # 🔷 Write CSV
 def write_csv(rows, output_path: Path):
@@ -34,17 +60,28 @@ def main():
     base_dir = Path(__file__).resolve().parent
     input_dir = base_dir / "input"
 
-    input_dir.mkdir(exist_ok=True)
-
+    n=1
     timestamp = datetime.now().strftime("%d-%m_%H-%M")
-    ordername = f"orders_{timestamp}_1.csv"
-    while (input_dir / ordername).exists():
-        ordername = f"orders_{timestamp}_{int(ordername.split('_')[-1].removesuffix('.csv')) + 1}.csv"
-    output_path = input_dir / ordername
+    orderfoldername = f"orders_{timestamp}_{n}"
+    
+    while (input_dir / orderfoldername).exists():
+        n=n+1
+        orderfoldername = f"orders_{timestamp}_{n}"
+
+    ordername_csv = f"_orders_{timestamp}_{n}.csv"
+        
+    order_dir = input_dir / orderfoldername
+    order_dir.mkdir(parents=True, exist_ok=True)
+
+    output_path_csv = order_dir / ordername_csv
+    output_path_settingsjson = order_dir / "settings.json"
+    output_path_disruptionjson = order_dir / "disruption.json"
 
     rows = generate_order()
-    write_csv(rows, output_path)
-    print(f"--- Created order file: {ordername} ----")
+    write_csv(rows, output_path_csv)
+    create_json(output_path_settingsjson)
+    create_json(output_path_disruptionjson)
+    print(f"--- Created order file: {orderfoldername} ----")
 
 
 if __name__ == "__main__":
