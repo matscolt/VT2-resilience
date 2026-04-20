0fcf57815ac7f61c656a5da5fb532c1f49f94740
Matscolt <mathias.terndrup@live.dk>
2026-04-20 13:12:24 +0200

    created the graph script

---
+++ production_line_sim/graphgen_postsim.py
@@ -0,0 +1,20 @@
+import matplotlib
+import csv
+
+#Creates plots from the data
+
+#import data
+
+#read csv
+
+#read json
+
+#plot functions
+
+
+#main
+def main():
+    print("this is the main function")
+
+if __name__ == "__main__":
+    main()
