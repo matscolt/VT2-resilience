cf1ac5fd82f1a8303b71e76e4e1b638efc2654ba
Matscolt <mathias.terndrup@live.dk>
2026-05-21 14:56:34 +0200

    fixed minimum dude date

--- production_line_sim/A_input.py
+++ production_line_sim/A_input.py
@@ -15,7 +15,7 @@ BASE_DIR = Path(__file__).parent
 data_dir = BASE_DIR / "data"
 PRIO_LOW = 1
 PRIO_HIGH = 5
-AVE_CYCLE_TIME_PER_UNIT = 76.4  # seconds per unit, used for due date generation
+AVE_CYCLE_TIME_PER_UNIT = 76.4*4  # seconds per unit, used for due date generation
 
 # ============================================================
 # CSV writers
@@ -273,7 +273,8 @@ def generate_orderlist(seed, plan_time, output_path: Path,num_orders, num_units)
         if order_id == num_orders:
             units = units_left
         units_left -= units
-        due_date = round_half_up(random.uniform(min(units * AVE_CYCLE_TIME_PER_UNIT*1.2, plan_time), plan_time))
+        print(f"units: {units} \nminimum due date: {units*AVE_CYCLE_TIME_PER_UNIT}")
+        due_date = round_half_up(random.uniform(min(units * AVE_CYCLE_TIME_PER_UNIT, plan_time), plan_time))
         priority = round_half_up(min(max(random.expovariate(1/1.5), PRIO_LOW), PRIO_HIGH))
         variant0 = "FUSE0"
         quantity0 = round_half_up(max(random.normalvariate(units * 0.33, unitstd), 0))
