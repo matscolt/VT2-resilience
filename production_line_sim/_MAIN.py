4a006f38e21a6956d6c6123e9a27c96aeac7bc03
Matscolt <mathias.terndrup@live.dk>
2026-05-23 10:40:20 +0200

    added time counter

--- production_line_sim/_MAIN.py
+++ production_line_sim/_MAIN.py
@@ -310,6 +310,7 @@ def main():
             end = ti.perf_counter()
             print(f"[MAIN] segment wall time: {end - start}\n\n\n - - - - - \n")
             print(f"the simulation has run for {int(end - run_time_start)} seconds")
+            print(f"Time since starting program: {end-main_start_time}")
             #input("Press [ENTER] to continue the loop")
         run_time_end = ti.perf_counter()
         print(f"\n\n--- RUN {run_idx} ---")
@@ -324,7 +325,12 @@ def main():
         next_pct = G_after_movie.progress_update(run_idx, max_idx, next_pct,action=action)
         #stop the loop
     main_stop_time = ti.perf_counter()
-    print(f"TOTAL TIME RUNNING MAIN\n{main_stop_time-main_start_time}")
+    total_time = main_stop_time-main_start_time 
+    h = total_time // 3600
+    m = (total_time % 3600) // 60
+    s = total_time % 60
+    clock_time = f"{h:d}:{m:02d}:{s:02d}"if h > 0 else f"{m:02d}:{s:02d}"
+    print(f"TOTAL TIME RUNNING MAIN\n{clock_time}")
 
 if __name__ == "__main__":
    main()
